73d3db019681a2474ce8638d0f8e03ed6b2fa230
LucasKinoshita <lucas_kinoshita@hotmail.com>
2023-05-25 11:29:37 -0300

    feat: add Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem 0;
+
+  > img {
+    width: 15rem;
+  }
+`
